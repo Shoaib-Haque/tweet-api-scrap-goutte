d102a568586ea86d115ba7a7df59bf89b3461fe7
XI-SHB <khchow@xandi.co.jp>
2022-10-05 13:17:18 +0600

    added followers api

--- app/Http/Controllers/TweetApiController.php
+++ app/Http/Controllers/TweetApiController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class TweetApiController extends Controller
 {
-    //
+    public function followers()
+    {
+        $client = new \GuzzleHttp\Client();
+        $id = 2244994945;
+        $endpoint = "https://api.twitter.com/2/users/{$id}/followers";
+
+        try {
+            $response = $client->request('GET', $endpoint, ['headers' => [
+                'authorization' =>
+                'Bearer aFNqQnNMaUMwWVI5Nm1OUkp3TndINUh2aEsxRWFJeXhva1Z3N3E3aVR4VEVoOjE2NjQ5NDc0Mzk4ODU6MToxOmF0OjE'
+            ]]);
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode == 200) {
+                $content = json_decode($response->getBody(), true);
+                $followers = $content['data'];
+                dd($followers);
+            } else {
+                if (env('APP_DEBUG')) {
+                    dd($statusCode);
+                } else {
+                    echo "Error!";
+                }
+            }
+        } catch (Exception $e) {
+            if (env('APP_DEBUG')) {
+                echo $e->getMessage();
+            } else {
+                echo "Error!";
+            }
+        }
+    }
 }
