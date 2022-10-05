a8c4be0693b41f4000f07640d9ba830eb060ae53
XI-SHB <khchow@xandi.co.jp>
2022-10-05 13:21:17 +0600

    added tweet liking api

--- app/Http/Controllers/TweetApiController.php
+++ app/Http/Controllers/TweetApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
 
 class TweetApiController extends Controller
 {
@@ -68,6 +69,68 @@ class TweetApiController extends Controller
                 echo $e->getMessage();
             } else {
                 echo "Error!";
+            }
+        }
+    }
+
+    public function likes()
+    {
+        $client = new \GuzzleHttp\Client();
+        $id = 2244994945;
+        $endpoint1 = "https://api.twitter.com/2/users/{$id}/tweets";
+        $count = 0;
+
+        try {
+            $response1 = $client->request('GET', $endpoint1, ['headers' => [
+                'authorization' =>
+                'Bearer aHhUUDFtT29sSldBbFJoVm05ZktfbWNERDJkRVFEb3dQLV9rbTVXcWZBdVVZOjE2NjQ5NTQ0MTIwMTE6MTowOmF0OjE'
+            ]]);
+            $statusCode1 = $response1->getStatusCode();
+
+            if ($statusCode1 == 200) {
+                $content1 = json_decode($response1->getBody(), true);
+                $tweets = $content1['data'];
+                foreach ($tweets as $key => $tweet) {
+                    $endpoint2 = "https://api.twitter.com/2/tweets/{$tweet['id']}/liking_users";
+                    try {
+                        $response2 = $client->request('GET', $endpoint2, ['headers' => [
+                            'authorization' =>
+                            'Bearer aHhUUDFtT29sSldBbFJoVm05ZktfbWNERDJkRVFEb3dQLV9rbTVXcWZBdVVZOjE2NjQ5NTQ0MTIwMTE6MTowOmF0OjE'
+                        ]]);
+                        $statusCode2 = $response2->getStatusCode();
+
+                        if ($statusCode2 == 200) {
+                            $content2 = json_decode($response2->getBody(), true);
+                            $likes = $content2['data'];
+                            $count += count($likes);
+                        } else {
+                            if (env('APP_DEBUG')) {
+                                dd($statusCode2);
+                            } else {
+                                echo "Error!";
+                            }
+                        }
+                    } catch (Exception $e) {
+                        if (env('APP_DEBUG')) {
+                            echo $e->getMessage();
+                        } else {
+                            echo "Error fetching likes!";
+                        }
+                    }
+                }
+                echo 'Total likes: ' . $count;
+            } else {
+                if (env('APP_DEBUG')) {
+                    dd($statusCode1);
+                } else {
+                    echo "Error!";
+                }
+            }
+        } catch (Exception $e) {
+            if (env('APP_DEBUG')) {
+                echo $e->getMessage();
+            } else {
+                echo "Error fetching tweets!";
             }
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 
 Route::get('/followers', 'App\Http\Controllers\TweetApiController@followers');
 Route::get('/tweets', 'App\Http\Controllers\TweetApiController@tweets');
+Route::get('/likes', 'App\Http\Controllers\TweetApiController@likes');
